558f35c2a6ee7db12d9b584bc702972b3bbaa1a2
Davi Hilário <davihilario23@gmail.com>
2025-05-19 21:24:16 -0300

    feat: criando lista de emojis e primeira versao do analisador lexico

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
 from unidecode import unidecode
+from palavras import *
+
+multiplicador = 1.0
 
 def run():
-    print(remove_acentos('café é muito bom'))
+
+    frase = 'Vai tomar no cu Enel, krl! que ódio'
+    palavras = frase.split(' ')
+
+    lista_filtrada = []
+
+    for palavra in palavras:
+
+        palavra = remove_acentos(palavra.lower())
+        palavra_invalida = False
+
+        if palavra not in PALAVRAS_NAO_UTILIZADAS:
+
+            for letra in palavra:
+                if letra in [NUMEROS, SIMBOLOS, (EMOJIS[i]['emoji'] for i in range(len(EMOJIS)))]:
+                    palavra_invalida = True
+                    break
+        
+        else:
+            palavra_invalida = True
+        
+        if not palavra_invalida:
+                lista_filtrada.append(palavra)
+        
+    print(lista_filtrada)
 
 def remove_acentos(palavra):
     return unidecode(palavra)
