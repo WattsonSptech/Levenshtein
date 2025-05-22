05739ce3c2b1f5d30c99cb3b0ad94189b26ae4c0
Davi Hilário <davihilario23@gmail.com>
2025-05-22 18:50:01 -0300

    feat: finalizada primeira versao do analisar de sentimentos

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from unidecode import unidecode
 from palavras import *
 
-multiplicador = 1.0
-pontos = 0
+sentimentos_por_frase = []
 
 def filtrar_frase(frase: str) -> list:
 
@@ -37,24 +36,34 @@ def tokenizer(palavras: list) -> list:
 
     return tokens
 
-    # for palavra in palavras:
-    #     if palavra in PALAVRAS_BOAS:
-    #         pontos = pontos + 1 * multiplicador
-    #         multiplicador = 1.0
-    #     elif palavra in PALAVRAS_RUINS:
-    #         pontos = pontos + (-1 * multiplicador)
-    #         multiplicador = 1.0
-    #     elif palavra in INTENSIFICADORES_POSITIVOS + INTENSIFICADORES_NEGATIVOS:
-    #         multiplicador += 1
-    
+def definir_sentimento(tokens: list) -> str:
+    multiplicador = 1.0
+    pontos = 0
 
+    for token in tokens:
+        if token[1] == "PSTV":
+            pontos = pontos + 1 * multiplicador
+            multiplicador = 1.0
+        elif token[1] == "NGTV":
+            pontos = pontos + (-1 * multiplicador)
+            multiplicador = 1.0
+        elif token[1] == "MULT":
+            multiplicador += 1
 
-def remove_caracteres_especiais(palavra):
+    if (pontos == 0):
+        return "Neutro"
+    elif (pontos > 0): 
+        return "Positivo"
+    else:
+        return "Negativo"
+
+def remove_caracteres_especiais(palavra: str) -> str:
     return unidecode(palavra)
 
 if __name__ == "__main__":
     frase = 'Novamente acabou a energia aqui no bairro. Nem mesmo estava chovendo. Simplesmente acaba do nada. Já teve uns 10 picos de retorno, mas pisca e acaba, colocando em risco os eletrodomésticos da casa. É um total descaso. A conta vem nas alturas. O serviço é péssimo. Já queimou transformador do poste da outra esquina, menos de um ano depois, outro transformador de outra esquina, quando cai qualquer chuva, já sabemos que vai faltar luz, e hj, mesmo sem chuva já estamos a 3 hs sem energia. E só lamento que esta péssima empresa seja enfiada goela abaixo de nós moradores. Torço para que seja multada diariamente, que vaá embora o quanto antes. Não precisam me responder. Pois sei que vcs não tem a menor condição de pre😡star esse serviço. Vcs são a pior empresa que eu tive o desprazer de ser obrigado a contratar.'
     frase_filtrada = filtrar_frase(frase)
-
     tokens = tokenizer(frase_filtrada)
-    print(tokens)
+    sentimento = definir_sentimento(tokens)
+    sentimentos_por_frase.append({"frase": frase, "sentimento": sentimento})
+    print(sentimentos_por_frase)
